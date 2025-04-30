2575f590d8bb50b272d8efe5fdd88dc21cf8d1f4
woodyk <837937+woodyk@users.noreply.github.com>
2025-04-29 20:47:46 -0400

    MsWhite_SOULPACK_v2.1.zip

--- update.sh
+++ update.sh
@@ -4,10 +4,11 @@
 # Author: Wadih Khairallah
 # Description: 
 # Created: 2025-04-29 20:41:35
+# Modified: 2025-04-29 20:46:17
 
 zip=`ls -t ./Archive | head -1`
 
-unzip -o ${zip}
+unzip -o ./Archive/${zip}
 git add .
 git commit -m "${zip}"
 git push
