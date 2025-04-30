7c1dfeff477d6a13567433beff8c243a135262d5
woodyk <837937+woodyk@users.noreply.github.com>
2025-04-29 20:18:33 -0400

    update

--- tests/test_boot.py
+++ tests/test_boot.py
@@ -1,4 +1,2 @@
-import importlib
-def test_boot():
-    sb = importlib.import_module('soulboot')
-    assert sb.boot() is True
+import importlib, pytest
+assert importlib.import_module('soulboot').boot() is True
